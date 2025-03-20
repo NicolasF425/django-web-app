a1b46bfb877fb683ae6ba2beb7f6fcbed5cb5c3f
NicolasF425 <nifan42@gmail.com>
2025-03-20 17:19:20 +0100

    commit 2

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,9 +1,39 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from listings.models import Band, Listing
+
 
 def hello(request):
-    return HttpResponse('<h1>Hello Django!</h1>')
+    bands = Band.objects.all()
+    return HttpResponse(f"""
+        <h1>Hello Django !</h1>
+        <p>Mes groupes préférés sont :</p>
+        <ul>
+            <li>{bands[0].name}</li>
+            <li>{bands[1].name}</li>
+            <li>{bands[2].name}</li>
+        </ul>
+    """)
+
 
 def about(request):
     return HttpResponse('<h1>A propos</h1> <p>Nous adorons merch !</p>')
+
+
+def listings(request):
+    listings = Listing.objects.all()
+    return HttpResponse(f"""
+        <h1>Listings</h1>
+        <p>Mes objets préférés sont :</p>
+        <ul>
+            <li>{listings[0].title}</li>
+            <li>{listings[1].title}</li>
+            <li>{listings[2].title}</li>
+            <li>{listings[3].title}</li>
+        </ul>
+    """)
+
+
+def contact(request):
+    return HttpResponse('<h1><p>Pour nous contacter</p></h1')
